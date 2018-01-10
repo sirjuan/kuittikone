d3bb03302aa138a2381e7db16e836ce25d1c6c8e
sirjuan <t6peju01@students.oamk.fi>
2018-01-10 14:11:23 +0200

    Delete unnecessary

--- src/Dashboard/reducer.js
+++
@@ -1,8 +0,0 @@
-import { ADD_TOP10_ITEMS } from 'store/constants'
-
-export const top10ItemsReducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TOP10_ITEMS: return action.top10Items
-    default: return state
-  }
-}
